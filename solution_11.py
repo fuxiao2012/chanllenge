8c1c0d456c23845099206dd0f8bb6f788472947f
fuxiao2012 <85433934@qq.com>
2016-02-04 15:10:21 +0800

    add
    first add

---
+++ solution_11.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+import string
+import re
+
+a = ['1', '11', '21', '1211', '111221']
+for i in xrange(5, 31):
+    nums=a[i-1]
+    init=nums[0]
+    cnt=1
+    rst=''
+    for j in nums[1:]:
+        if j == init:
+            cnt+=1
+            continue
+        rst+=str(cnt)+init
+        init=j
+        cnt=1    
+    rst+=str(cnt)+init    
+    a.append(rst)
+    #print "%d:%s " % (i, a[i])
+print len(a[30])    
